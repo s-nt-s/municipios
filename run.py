5931640a7b442651557b9f8242203b74c76d6468
s-nt-s <santos82h@gmail.com>
2019-06-20 16:09:55 +0200

    reload

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from core.db import DBMun, plain_parse_col
 
 dataset = Dataset()
 dataset.unzip()
-db = DBMun(reload=False, parse_col=plain_parse_col)#file="dataset/municipios.db")
+db = DBMun(reload=True, parse_col=plain_parse_col)#file="dataset/municipios.db")
 dataset.populate_datamun(db)
 db.close()
 db.zip()
