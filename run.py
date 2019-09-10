107bcb8529fe9ef0925f04ba48e26d46adb35cc2
s-nt-s <santos82h@gmail.com>
2019-09-10 11:16:46 +0200

    logging.WARNING como nivel de depuración por defecto

--- run.py
+++ run.py
@@ -7,12 +7,14 @@ from core.create_db import create_db
 
 parser = argparse.ArgumentParser("Crea base de datos de municipios")
 parser.add_argument('--verbose', '-v', action='count',
-                    help="Nivel de depuración")
+                    help="Nivel de depuración", default=0)
 parser.add_argument('salida', nargs='?',
                     help='Nombre de la base de datos de salida', default="dataset/municipios.db")
 args = parser.parse_args()
+
 levels = [logging.WARNING, logging.INFO, logging.DEBUG]
 level = levels[min(len(levels)-1, args.verbose)]
+
 logging.basicConfig(
     level=level, format='%(asctime)s - %(levelname)s - %(message)s')
 
